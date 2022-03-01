53acf8525d33aedeca455d9a21a77d8efff3b537
José Carlos Valeriano Rodríguez <78151884+JoseCarlosValeriano@users.noreply.github.com>
2022-03-01 14:10:23 +0000

    Feat: Se ha realizado la conexión remota pidiendo al usuario el usuario y la contraseña

--- src/com/programaciondeservicios/ssh/Main.java
+++ src/com/programaciondeservicios/ssh/Main.java
@@ -1,8 +1,44 @@
 package com.programaciondeservicios.ssh;
 
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Session session = null;
+        ChannelExec channel = null;
+        Scanner scanner = new Scanner(System.in);
+
+        try{
+            System.out.printf("Introduce el nombre de usuario:");
+            String username = scanner.nextLine();
+
+            System.out.printf("Introduce la contraseña del usuario:");
+            String password = scanner.nextLine();
+
+            session = new JSch().getSession(username, "127.0.0.1", 22);
+            session.setPassword(password);
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.connect();
+            channel = (ChannelExec) session.openChannel("exec");
+
+            System.out.println("Conexión remota conectada correctamente");
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if (session != null){
+                session.disconnect();
+            }
+            if (channel != null){
+                channel.disconnect();
+            }
+        }
+
+
 
     }
 }
